3088eee83b57cb8eb0bc1fe890ed9fa7f29dabb8
M0nster92 <fahimalsafi@gmail.com>
2022-05-15 11:10:02 -0400

    changed the file structure

--- features/fetchjson.js
+++ features/fetchjson.js
@@ -3,5 +3,8 @@ exports.__esModule = true;
 var axios_1 = require("axios");
 var url = 'https://jsonplaceholder.typicode.com/todos/1';
 axios_1["default"].get(url).then(function (res) {
-    console.log(res.data);
+    var todo = res.data;
+    var ID = todo.id;
+    var title = todo.title;
+    console.log("\n        id: ".concat(ID, "\n        title: ").concat(title, "\n    "));
 });
