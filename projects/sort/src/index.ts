1ffd3522291585f8a55d345bd1c6501ec116e825
M0nster92 <fahimalsafi@gmail.com>
2022-06-08 11:14:57 -0400

    completed sort project

--- projects/sort/src/index.ts
+++ projects/sort/src/index.ts
@@ -1,13 +1,23 @@
 import { Sorter } from "./Sorter";
 import { NumberCollection } from "./NumbersCollection";
 import { CharacterCollection } from "./CharacterCollection";
+import { LinkedList } from "./LinkedList";
 
 //const numberCollection = new NumberCollection([10, 3, -5, 0]);
 //const sorter = new Sorter(numberCollection);
-//sorter.sort();
+//numberCollection.sort();
 //console.log(numberCollection.data);
 
-const characterCollection = new CharacterCollection("raqbj");
-const sorter = new Sorter(characterCollection);
-sorter.sort();
-console.log(characterCollection.data);
+//const characterCollection = new CharacterCollection("raqbj");
+//const sorter = new Sorter(characterCollection);
+//sorter.sort();
+//console.log(characterCollection.data);
+
+const linkList = new LinkedList();
+linkList.add(500);
+linkList.add(-10);
+linkList.add(-3);
+linkList.add(4);
+
+linkList.sort();
+linkList.print();
