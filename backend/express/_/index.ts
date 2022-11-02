47664df6a2408ad3285e5e9f6621b37142b89c7b
M0nster92 <fahimalsafi@gmail.com>
2022-11-01 19:16:20 -0600

    sync

--- backend/express/_/index.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import reminderRouter from "./router/reminders";
-
-const app = express();
-app.use(express.json());
-
-app.use("/reminders", reminderRouter);
-
-app.listen(3000, () => console.log("Server started"));
--- backend/express/_/models/reminders.ts
+++
@@ -1,8 +0,0 @@
-export default class Reminder {
-  _id: number;
-  isCompleted: boolean;
-  constructor(public title: string) {
-    this._id = Date.now();
-    this.isCompleted = false;
-  }
-}
